d76aaa4b7fc3c7ad9333217418615e78f81bf465
Sakib-Anjum-313 <sakibanjum.313@gmail.com>
2023-01-21 12:32:38 +0600

    Env setup done

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCu-ZfBBtPl5LxTUdeFcl4CgfBRCUC-xkg",
-  authDomain: "bohubrihi-learning-app.firebaseapp.com",
-  projectId: "bohubrihi-learning-app",
-  storageBucket: "bohubrihi-learning-app.appspot.com",
-  messagingSenderId: "966287304051",
-  appId: "1:966287304051:web:5d5e741d158ce7451b5106",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
